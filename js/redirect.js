5e2735c61748e1ec4eb0abb6c9688c6a78ca4f42
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-03-09 20:16:31 -0300

    feat: react project initialized

--- js/redirect.js
+++
@@ -1,6 +0,0 @@
-var userLang = navigator.language || navigator.userLanguage; 
-if (userLang == "pt-BR" || userLang == "pt"){
-    window.location = "pt-br/index.html";
-} else {
-    window.location = "eng/index.html";
-}
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <Router>
+      <Route exact path="/">
+        a
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
