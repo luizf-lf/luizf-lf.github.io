e8bcf399c0d85ff0f5ed94b17577334e4dfc12fe
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-03-11 15:52:31 -0300

    feat: home page added

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import styles from '../styles/Homepage.module.css';
+
+import { ReactComponent as MailIcon } from '../assets/mail.svg';
+
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.homepageContainer}>
+      <p></p>
+      <div className={styles.presentationContainer}>
+        <div className={styles.presentationText}>
+          <strong>Desenvolvendo um novo mundo.</strong>
+          <p>Uma linha de código por vez.</p>
+        </div>
+        <div className={styles.presentationAbout}>
+          <h1>Luiz Fernando</h1>
+          <p>Full Stack Web Developer</p>
+          <p>Minas Gerais, Brasil</p>
+          <div className={styles.presentationStack}>Stack</div>
+        </div>
+      </div>
+      <button type="button" className={styles.footerButton}>
+        <MailIcon className={styles.footerButtonSvg} />
+        Entre em contato
+      </button>
+    </div>
+  );
 }
 
 export default Homepage;
