19a0a28264f96d551d815d87f7fd3cd30c148fb3
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-04-14 20:56:22 -0300

    feat: added more items to skill stack

--- src/components/homepage/SkillStackItem.tsx
+++ src/components/homepage/SkillStackItem.tsx
@@ -37,16 +37,27 @@ const SkillStackItem = ({ name, level, icon, color, role }: stackItemProps) => {
 
   for (let i = 0; i < 6; i++) {
     let color = '';
+    let shadowColor = '';
     if (level < 3) {
       color = '#ffe268';
+      shadowColor = 'rgba(255, 225, 104, 0.75)';
     } else if (level >= 3 && level <= 4) {
       color = '#51c4d3';
+      shadowColor = 'rgba(81, 196, 211, 0.75)';
     } else {
       color = '#8fd9a8';
+      shadowColor = 'rgba(143, 217, 168, 0.75)';
     }
 
     if (i < level) {
-      levelDots.push(<div style={{ backgroundColor: color }}></div>);
+      levelDots.push(
+        <div
+          style={{
+            backgroundColor: color,
+            boxShadow: `0px 0px 5px 1px ${shadowColor}`,
+          }}
+        ></div>
+      );
     } else {
       levelDots.push(<div></div>);
     }
