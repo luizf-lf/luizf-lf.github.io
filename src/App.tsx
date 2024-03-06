c340d2b909c2a276a0921b7c04eec0bc19c688f5
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2024-03-06 19:12:08 -0300

    feat: v1 content archived

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import './styles/global.css';
-import './i18n';
-
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Homepage from './pages/Homepage';
-import Loader from './components/Loader';
+import LayoutContainer from './v1/LayoutContainer';
 
 function App() {
-  return (
-    <Suspense fallback={<Loader />}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </Suspense>
-  );
+  return <LayoutContainer />;
 }
 
 export default App;
---
+++ src/v1/LayoutContainer.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import './styles/global.css';
+import './i18n';
+
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Homepage from './pages/Homepage';
+import Loader from './components/Loader';
+
+function LayoutContainer() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Suspense>
+  );
+}
+
+export default LayoutContainer;
